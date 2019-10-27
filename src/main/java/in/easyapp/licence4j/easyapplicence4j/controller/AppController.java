c1571b4b1ae34e3adcba14f2dcd3d3f399feaff7
mdaftabasmu <mdaftabalam196@gmail.com>
2019-10-27 23:00:41 +0530

    licence 4j

--- src/main/java/in/easyapp/licence4j/easyapplicence4j/controller/AppController.java
+++ src/main/java/in/easyapp/licence4j/easyapplicence4j/controller/AppController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import in.easyapp.licence4j.easyapplicence4j.request.LicenseResponse;
--- src/main/java/in/easyapp/licence4j/easyapplicence4j/service/impl/AppServiceImpl.java
+++ src/main/java/in/easyapp/licence4j/easyapplicence4j/service/impl/AppServiceImpl.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 
 import in.easyapp.licence4j.easyapplicence4j.request.ProjectMdl;
 import in.easyapp.licence4j.easyapplicence4j.service.AppService;
+import org.springframework.stereotype.Service;
 
 @Service
 public class AppServiceImpl implements AppService {
